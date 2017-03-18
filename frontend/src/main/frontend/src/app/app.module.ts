3923674c84d0f40c2ae469bac55c83a4ebd46d43
Mateusz Chudy (matechu268) <matechu268@student.polsl.pl>
2017-03-18 23:46:01 +0100

    Dodanie bootstrapa, wstępny szkielet aplikacji frontendu.

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -2,19 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GravatarComponent } from './components/gravatar/gravatar.component';
+import { UserComponent } from './components/user/user.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GravatarComponent,
+    UserComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    MaterialModule,
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NavbarComponent,
+    GravatarComponent,
+    UserComponent,
+    UserListComponent
+  ]
 })
 export class AppModule { }
---
+++ frontend/src/main/frontend/src/app/components/gravatar/gravatar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gravatar',
+  templateUrl: 'gravatar.component.html',
+  styleUrls: ['gravatar.component.css']
+})
+export class GravatarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
