1d702d7ab8cd9ab2fa0f2d4ca9a1cd362c300a74
Mateusz Chudy (matechu268) <matechu268@student.polsl.pl>
2017-04-05 01:08:02 +0200

    Usunięcie zbędnych plików.

--- frontend/src/main/frontend/src/app/dashboard/home/home.component.ts
+++ frontend/src/main/frontend/src/app/dashboard/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
-//import initDemo = require('../../../assets/js/charts.js');
+// import initDemo = require('../../../assets/js/charts.js');
 
 declare var $:any;
 
@@ -11,12 +11,12 @@ declare var $:any;
 
 export class HomeComponent implements OnInit{
     ngOnInit(){
-        // $('[data-toggle="checkbox"]').each(function () {
-        //     if($(this).data('toggle') == 'switch') return;
-        //
-        //     var $checkbox = $(this);
-        //     $checkbox.checkbox();
-        // });
-        //initDemo();
+        $('[data-toggle="checkbox"]').each(function () {
+            if($(this).data('toggle') == 'switch') return;
+
+            var $checkbox = $(this);
+            $checkbox.checkbox();
+        });
+        // initDemo();
     }
 }
