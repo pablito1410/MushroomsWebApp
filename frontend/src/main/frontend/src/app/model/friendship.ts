42232be2be1d0456e059a6ee4eda118108bbf62b
Mateusz Chudy (matechu268) <matechu268@student.polsl.pl>
2017-05-23 13:15:46 +0200

    Edycja serwisów znajomych.

--- frontend/src/main/frontend/src/app/model/friendship.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by MiKu on 22.05.2017.
- */
-
-export class Friendship {
-    userId : number;
-    friendId : number;
-}
--- frontend/src/main/frontend/src/app/services/friend.service.ts
+++ frontend/src/main/frontend/src/app/services/friend.service.ts
@@ -1,34 +1,20 @@
-/**
- * Created by MiKu on 22.05.2017.
- */
-
 import { Injectable } from '@angular/core';
-import {URLSearchParams, Http, Headers, RequestOptions, Response } from '@angular/http';
-
-import { User } from '../model/user';
-import {AuthenticationService} from "./authentication.service";
-import {Friendship} from "../model/friendship";
+import { Http, Response } from '@angular/http';
+import { AuthenticationService } from "./authentication.service";
 
 @Injectable()
 export class FriendService {
     constructor(private http: Http, private authenticationService: AuthenticationService) { }
 
-    getAllByUserId(id: number) {
-        return this.http.get('/api/friends/' + id, this.authenticationService.jwt()).map((response: Response) => response.json());
+    getAll() {
+        return this.http.get('/api/friends', this.authenticationService.jwt()).map((response: Response) => response.json());
     }
 
-    // getById(id: number) {
-    //     return this.http.get('/api/friends/' + id, this.authenticationService.jwt()).map((response: Response) => response.json());
-    // }
-
-    add(friendship: Friendship){
-        return this.http.post('/api/friends', friendship, this.authenticationService.jwt()).map((response : Response) => response.json());
+    create(id: number) {
+        return this.http.post('/api/friends', id, this.authenticationService.jwt()).map((response : Response) => response.json());
     }
 
-    delete(friendship: Friendship) {
-        let urlSearchParams = new URLSearchParams();
-        urlSearchParams.append('user',friendship.userId.toString());
-        urlSearchParams.append('friend',friendship.friendId.toString());
-        return this.http.delete('/api/friends/' + urlSearchParams.toString(), this.authenticationService.jwt()).map((response: Response) => response.json());
+    delete(id: number) {
+        return this.http.delete('/api/friends/' + id, this.authenticationService.jwt()).map((response: Response) => response.json());
     }
 }
