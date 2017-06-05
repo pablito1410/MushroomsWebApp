e0cbfa0fbef68c0f48c80983a57c6a31b4e69f44
Mateusz Chudy (matechu268) <matechu268@student.polsl.pl>
2017-06-05 03:31:23 +0200

    Uproszczenie komponentów logowania i rejestracji. Rozbudowanie okien dialogowych szczegółów znajomych i znalezisk. Inne poprawki.

--- frontend/src/main/frontend/src/app/dashboard/discoveries/discovery-details/discovery-details.component.ts
+++ frontend/src/main/frontend/src/app/dashboard/discoveries/discovery-details/discovery-details.component.ts
@@ -8,7 +8,16 @@ import {SearchFriendsComponent} from "../../friends/search-friends/search-friend
     templateUrl: 'discovery-details.component.html'
 })
 export class DiscoveryDetailsComponent implements OnInit {
+    discovery: any;
     constructor(public dialogRef: MdDialogRef<DiscoveryDetailsComponent>) { }
     ngOnInit() {
+        this.discovery = {
+            id: 1,
+            name: 'Podgrzybek',
+            coordinateX: 51.673858,
+            coordinateY: 7.815982,
+            photo: null,
+            date: '12.05.2017 9:44'
+        };
     }
 }
--- frontend/src/main/frontend/src/app/dashboard/friends/friend-details/friend-details.component.ts
+++ frontend/src/main/frontend/src/app/dashboard/friends/friend-details/friend-details.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Inject} from "@angular/core";
 import {MdDialogRef, MD_DIALOG_DATA} from "@angular/material";
+import {User} from "../../../model/user";
 
 @Component({
     moduleId: module.id,
@@ -7,7 +8,19 @@ import {MdDialogRef, MD_DIALOG_DATA} from "@angular/material";
     templateUrl: 'friend-details.component.html'
 })
 export class FriendDetailsComponent implements OnInit {
+    user: any;
     constructor(public dialogRef: MdDialogRef<FriendDetailsComponent>) { }
     ngOnInit() {
+        this.user = {
+            id: 1,
+            username: 'mati',
+            firstName: 'Mateusz',
+            lastName: 'Nowak',
+            email: 'email@email.com',
+            birthDate: '22.10.1995',
+            gender: 'male',
+            role: 'admin',
+            level: 'beginner'
+        };
     }
 }
