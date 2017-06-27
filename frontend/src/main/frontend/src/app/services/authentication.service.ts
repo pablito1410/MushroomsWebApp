de21bd019ac7cea0758764c8693dd11e6eb9822a
Mateusz Chudy (matechu268) <matechu268@student.polsl.pl>
2017-06-27 20:04:46 +0200

    Poprawka z tworzeniem usera i usunięcie zbędnego kodu.

--- frontend/src/main/frontend/src/app/services/authentication.service.ts
+++ frontend/src/main/frontend/src/app/services/authentication.service.ts
@@ -31,18 +31,6 @@ export class AuthenticationService {
             });
     }
 
-    // login(email: string, password: string) {
-    //     return this.http.post('/login', JSON.stringify({ email: email, password: password }))
-    //         .map((response: Response) => {
-    //             // login successful if there's a jwt token in the response
-    //             let user = response.json();
-    //             if (user && user.token) {
-    //                 // store user details and jwt token in local storage to keep user logged in between page refreshes
-    //                 localStorage.setItem('currentUser', JSON.stringify(user));
-    //             }
-    //         });
-    // }
-
     logout() {
         // remove user from local storage to log user out
         localStorage.removeItem('token');
