9726416c6458cc8f7b19190b4334b73d2aa4b816
chythe <matechu268@gmail.com>
2017-09-28 20:27:14 +0200

    Aktualizacja material angulara Odświeżanie co minutę navbara Dodanie do navbara miniaturki i username

--- frontend/src/main/frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/main/frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, OnChanges } from '@angular/core';
 import { ROUTES } from '../.././sidebar/sidebar-routes.config';
 import { MenuType } from '../.././sidebar/sidebar.metadata';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { AuthenticationService } from "../../services/authentication.service";
 import { NotificationService } from "../../services/notification.service";
+import { UserService } from "../../services/user.service";
 import { Notification } from "../../model/notification";
+import { User } from "../../model/user";
 import { MdSnackBar } from "@angular/material";
 import { DOCUMENT } from "@angular/platform-browser";
+import { Tools } from "../../tools/tools";
+import { Observable } from 'rxjs/Rx';
 
 /**
  * Navbar component
@@ -24,6 +28,11 @@ export class NavbarComponent implements OnInit {
     private arrayTitles: any[];
     /** Router location */
     location: Location;
+    /** Current user */
+    user: User;
+
+    /** Static method get photo string to display assignment */
+    getPhotoStringToDisplay = Tools.getPhotoStringToDisplay;
 
     /**
      * Constructor of class
@@ -31,14 +40,17 @@ export class NavbarComponent implements OnInit {
      * @param document                  Current document
      * @param authenticationService     Authentication service
      * @param notificationService       Notification service
+     * @param userService               User service
      */
     constructor(
         location: Location,
         @Inject(DOCUMENT) private document,
         private authenticationService: AuthenticationService,
-        private notificationService: NotificationService) {
+        private notificationService: NotificationService,
+        private userService: UserService) {
         this.notifications = new Array<Notification>();
         this.location = location;
+        this.user = new User();
     }
 
     /**
@@ -50,16 +62,31 @@ export class NavbarComponent implements OnInit {
             // for only frontend development purposes
             this.initFakeData();
         } else {
-            this.notificationService.getAll().subscribe(
-                result => this.notifications = result
-            );
+            this.getData();
+            // refresh any minute
+            Observable.interval(1000 * 60).subscribe(x => {
+                this.getData();
+            });
         }
+    }
+
+    /**
+     * Gets data from server
+     */
+    getData() {
+        this.userService.get().subscribe(
+            result => this.user = result
+        );
+        this.notificationService.getAll().subscribe(
+            result => this.notifications = result
+        );
     }
 
     /**
      * Initialize the component with fake data
      */
     private initFakeData() {
+        this.user.username = "mati";
         this.notifications = [
             {
                 id: 1,
